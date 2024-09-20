f9e4148ec52bd4eaec5e1b57c4f664a2f99a1211
Jacobo24 <jaccalgar@gmail.com>
2024-09-20 19:22:06 +0200

    :)

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import subsubclases.camioneta as camioneta
 if __name__ == '__main__':
     mi_vehiculo = coche("azul", 4, 150, 1200)
     print (mi_vehiculo)
-    mi_vehiculo2 = bicicleta("verde", 2, "urbana", "deportiva")
+    mi_vehiculo2 = bicicleta("verde", 2, "urbana",)
     print (mi_vehiculo2)
-    mi_vehiculo3 = camioneta("blanca", 4, 1000)
+    mi_vehiculo3 = camioneta("blanca", 4, 500, 1000, 2000)
     print (mi_vehiculo3)
-    mi_vehiculo4 = motocicleta("negra", 2, 180, 900)
+    mi_vehiculo4 = motocicleta("negra", "deportiva", 689, 2, 1000)
     print (mi_vehiculo4)
